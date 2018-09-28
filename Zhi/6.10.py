a3a8eb24f8e468c8aefaa601bfccd8e3ffa4a02e
Unknown <zhiwei_gan@hotmail.com>
2018-09-28 13:47:39 -0400

    changed location of file

--- Zhi/6.10.py
+++ Zhi/6.10.py
@@ -10,6 +10,12 @@ def process(sentence):
             featuresets.append(({'word':w3}, 'on'))
             #print(w1, w2, w3)
 
+        # if w1.lower() == 'on' and (t2.startswith('DT') or t2 == 'AT') and w3.lower() == 'computer':
+        #     #featuresets.append(({'word':w3}, 'on'))
+        #     print(sentence)
+    
+
+
 
 for tagged_sent in brown.tagged_sents():
     process(tagged_sent)
@@ -17,4 +23,5 @@ for tagged_sent in brown.tagged_sents():
 train_set, test_set = featuresets[500:], featuresets[:500]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
 #print(classifier.show_most_informative_features(10))
+#print(nltk.classify.accuracy(classifier, test_set))
 print(classifier.classify({'word':input("Enter the word please: ").lower()}))
